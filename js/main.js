548301b7fb250071555b47c5362d4e054bc30360
Evandro F Lima <evandrofelipe1333@gmail.com>
2023-04-21 15:57:59 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,45 +1,74 @@
-const timer = document.querySelector('.timer');
-let second = 0;
-let control;
+function createCalculator(){
+    return{
+        display: document.querySelector('.display'),
 
 
-document.addEventListener('click',function(event){
-    const element = event.target;
 
-    if(element.classList.contains('start'))
-    {
-        clearInterval(control);
-        addSecond()
-        timer.classList.remove('red');
+        inicia(){
+            this.clickBtn();
+            this.enterPress();
+        },
+
+        clickBtn(){
+            document.addEventListener('click', function(event){
+                const element = event.target;
+                if(element.classList.contains('num')){
+                    this.btnForDisplay(element.innerText);
+                }
+                if(element.classList.contains('clear')){
+                    this.clearDisplay();
+                }
+                if(element.classList.contains('del')){
+                    this.delet();
+                }
+                if(element.classList.contains('equal')){
+                    this.equal();
+                }
+            }.bind(this) );
+        },
+
+        enterPress(){
+            this.display.addEventListener('keyup', event => {
+                if(event.keyCode === 13){
+                    this.equal();
+                }
+            });
+        },
+
+        btnForDisplay(valor){
+            this.display.value += valor;
+        },
+
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        delet(){
+            this.display.value = this.display.value.slice(0, -1);
+           
+        },
+
+        equal(){
+            let result = this.display.value;
+            try { 
+                result = eval(result);
+
+                if(!result){
+                    alert('Invalid');
+                    return;
+                }
+
+            this.display.value = result;
+
+            }
+
+            catch(e){
+                alert('Invalid');
+                return;
+            }
+        }
     }
-
-    if(element.classList.contains('pause'))
-    {
-        clearInterval(control);
-        timer.classList.add('red');
-    }
-
-    if(element.classList.contains('reset'))
-    {
-        clearInterval(control);
-        second = 0
-        timer.classList.remove('red');
-        timer.innerHTML = '00:00:00';
-    }
-
-});
-
-function addSecond()
-{
-    control = setInterval(function () {
-        second++;
-        timer.innerHTML = setHourSeconds(second);
-    }, 1000)
 }
-function setHourSeconds(second) {
-    const date_ = new Date(second * 1000);
-    return date_.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'GMT'
-    })
-}
+
+const calculadora = createCalculator();
+calculadora.inicia();
